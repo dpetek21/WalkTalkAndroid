5e81801cd03f12aee9644460af3bf69d5b711a01
Marin Puškadija <mpuskadij20@foi.hr>
2023-10-30 19:55:58 +0100

    Dodan string resurs za dolazeće zahtjevedodana klasa PendingFriendsFragment, stvorena adapter klasa FriendsAdapter te implementirana metoda createFragment #4

--- Software/app/src/main/java/hr/foi/rampu/walktalk/adapters/FriendsAdapter.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/adapters/FriendsAdapter.kt
@@ -4,13 +4,21 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import hr.foi.rampu.walktalk.R
+import hr.foi.rampu.walktalk.fragments.CurrentFriendsFragment
+import hr.foi.rampu.walktalk.fragments.PendingFriendsFragment
 
 class FriendsAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager,lifecycle) {
+
+    val titles = listOf(R.string.friends,R.string.pending_friend_requests)
     override fun getItemCount(): Int {
         TODO("Not yet implemented")
     }
 
     override fun createFragment(position: Int): Fragment {
-        TODO("Not yet implemented")
+        return when(position) {
+            0 -> CurrentFriendsFragment()
+            else -> PendingFriendsFragment()
+        }
     }
 }
