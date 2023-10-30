67d7f72c8bbd5aa266012444d39d68378939179e
Marin Puškadija <mpuskadij20@foi.hr>
2023-10-30 18:10:41 +0100

    Dodana povezansot sa Firebase bazom podataka i promjena SDK-a za kompialciju #4
    google-services.json je generirao Firebase i traži da se stavi u appdirektorij
    
    U gradle datoteke sam stavio plugin(projekt i app) i 2 nove ovisnosti(app)
    
    Projekt je inicijalno koristio compileSDK 33, a sad je potrebno koristiti 34

--- Software/app/build.gradle.kts
+++ Software/app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "hr.foi.rampu.walktalk"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "hr.foi.rampu.walktalk"
@@ -36,12 +37,14 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.4.1"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
