75a438bafd6c991969a1df69acb95ef8fc6e28c8
Marin Puškadija <mpuskadij20@foi.hr>
2023-10-31 17:17:20 +0100

    Povezan tab layout sa viewPagerom preko TabLayoutMediator-a te dodane ikone za svaki tab #4

--- Software/app/src/main/java/hr/foi/rampu/walktalk/FriendsActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/FriendsActivity.kt
@@ -2,21 +2,34 @@ package hr.foi.rampu.walktalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.toDrawable
 import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
-import hr.foi.rampu.walktalk.adapters.FriendsPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class FriendsActivity : AppCompatActivity() {
 
-    private lateinit var tab : TabLayout
-    private lateinit var viewPager: ViewPager
+    private lateinit var tabLayout : TabLayout
+    private lateinit var viewPager: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friends)
 
-        tab = findViewById(R.id.current_incoming_friendsTab)
-        viewPager = findViewById(R.id.vpFriends)
+        tabLayout = findViewById(R.id.current_incoming_friendsTab)
+        viewPager = findViewById(R.id.vp2Friends)
 
-
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = R.string.friends.toString()
+                    tab.icon = R.drawable.ic_friends.toDrawable()
+                }
+                1 -> {
+                    tab.text = R.string.pending_friend_requests.toString()
+                    tab.icon = R.drawable.ic_friend_request_pending.toDrawable()
+                }
+            }
+        }.attach()
     }
 }
