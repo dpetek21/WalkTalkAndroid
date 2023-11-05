ebcac3c4546ccb9b4ae9e82a8dca2e0be1493ccb
Marin Puškadija <mpuskadij20@foi.hr>
2023-11-05 10:23:53 +0100

    Pregazi metodu onViewCreated za povezivanje varijable sa reciklirajućim pogledom  #4

--- Software/app/src/main/java/hr/foi/rampu/walktalk/fragments/CurrentFriendsFragment.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/fragments/CurrentFriendsFragment.kt
@@ -5,14 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rampu.walktalk.R
 
 class CurrentFriendsFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_current_friends, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = view.findViewById(R.id.rv_current_friends)
     }
 }
