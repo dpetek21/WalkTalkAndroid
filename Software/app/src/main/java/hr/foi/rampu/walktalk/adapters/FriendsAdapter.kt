99973ab141cb52936c73426122dc2e10d8b276f9
Marin Puškadija <mpuskadij20@foi.hr>
2023-11-06 15:00:50 +0100

    Dovrši kod unutarnje klase FriendsViewHolder i onBindViewHolder #4

--- Software/app/src/main/java/hr/foi/rampu/walktalk/adapters/FriendsAdapter.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/adapters/FriendsAdapter.kt
@@ -3,6 +3,7 @@ package hr.foi.rampu.walktalk.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rampu.walktalk.R
 import hr.foi.rampu.walktalk.entities.Friend
@@ -10,7 +11,15 @@ import hr.foi.rampu.walktalk.entities.Friend
 class FriendsAdapter(val friendsList : List<Friend>) : RecyclerView.Adapter<FriendsAdapter.FriendsViewHolder>() {
     inner class FriendsViewHolder(view: View) : RecyclerView.ViewHolder(view)
     {
+        private val username : TextView
+        init {
+            username = view.findViewById(R.id.txtv_username)
+        }
 
+        fun bind(friend: Friend)
+        {
+            username.text = friend.username
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FriendsViewHolder {
@@ -25,6 +34,6 @@ class FriendsAdapter(val friendsList : List<Friend>) : RecyclerView.Adapter<Frie
     }
 
     override fun onBindViewHolder(holder: FriendsViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(friendsList[position])
     }
 }
