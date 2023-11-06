2f701f87d4059f57e869f53a6b161fb3dc8ca6db
Gabriel Vesel <gvesel20@foi.hr>
2023-11-06 17:32:29 +0100

    Dodan adapter za recycle view koji pokazuje poruke.

---
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/adapteri_za_chat/MessageAdapter.kt
@@ -0,0 +1,29 @@
+package hr.foi.rampu.walktalk.adapteri_za_chat
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MessageAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    class SentMessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+
+    class ReceiveMessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+
+}
