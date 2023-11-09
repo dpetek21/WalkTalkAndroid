3d6f997869d0686737de5f87cdc9d155efa71371
Marin Puškadija <mpuskadij20@foi.hr>
2023-11-09 19:29:55 +0100

    Dodaj zasebnu listu za nadolazeće zahtjeve #4

--- Software/app/src/main/java/hr/foi/rampu/walktalk/helpers/MockFriends.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/helpers/MockFriends.kt
@@ -4,4 +4,5 @@ import hr.foi.rampu.walktalk.entities.Friend
 
 object MockFriends {
     fun getFriends() : List<Friend> = listOf(Friend("marin"), Friend("bojan"), Friend("igor"))
+    fun getPendingFriends() : MutableList<Friend> = mutableListOf(Friend("Domagoj"), Friend("Gabriel"), Friend("Lea"))
 }
