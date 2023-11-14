dc94f9fea7aa70963eb33e45f83e9a35107c5fd5
Gabriel Vesel <gvesel20@foi.hr>
2023-11-14 18:02:38 +0100

    Izmijenjen kod da se poruke spremaju u Firestore database a ne u Realtime database.

--- Software/app/src/main/java/hr/foi/rampu/walktalk/PrivateChatActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/PrivateChatActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.Firebase
 import com.google.firebase.database.DataSnapshot
@@ -12,28 +13,30 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
+import com.google.firebase.firestore.FirebaseFirestore
 import hr.foi.rampu.walktalk.klase_za_chat.Message
+import java.util.UUID
 
 class PrivateChatActivity : AppCompatActivity() {
 
     private lateinit var chatText: EditText
     private lateinit var sendButton: ImageButton
     private lateinit var chatRecyclerView: RecyclerView
-    private lateinit var databaseReference: DatabaseReference
     private lateinit var sender: String
     private lateinit var receiver: String
+    private lateinit var database: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_private_chat)
 
         chatText = findViewById<EditText>(R.id.edt_private_chat_massage)
-        databaseReference = FirebaseDatabase.getInstance("https://rampu---walk-talk-default-rtdb.europe-west1.firebasedatabase.app/").reference
         sendButton = findViewById<ImageButton>(R.id.imageButton_send)
-
+        database = FirebaseFirestore.getInstance()
 
         sendButton.setOnClickListener{
-            sendMessage();
+            sendMessage()
+            chatText.text.clear()
         }
 
     }
@@ -41,12 +44,19 @@ class PrivateChatActivity : AppCompatActivity() {
     private fun sendMessage(){
         val text = chatText.text
         if(text.toString() != ""){
-            val message = Message(text.toString(), "1", "5")
 
-            val checkForNode = FirebaseDatabase.getInstance("https://rampu---walk-talk-default-rtdb.europe-west1.firebasedatabase.app/")
-                .getReference("/messages/Sender: ${message.sender}, Receiver: ${message.receiver}")
-            checkForNode.push().setValue(message)
-            text.clear()
+            val message = Message(text.toString(), "1", "2")
+            val users = hashMapOf(
+                "User1" to message.sender,
+                "User2" to message.receiver
+            )
+            val messageChannel = database.collection("messages").document(UUID.randomUUID().toString())
+            messageChannel.set(users).addOnSuccessListener {
+                messageChannel.collection("private_messages")
+                    .document(UUID.randomUUID().toString())
+                    .set(message)
+            }
+
         }
     }
 
