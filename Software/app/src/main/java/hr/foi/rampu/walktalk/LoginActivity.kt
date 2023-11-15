dfdf84781155517f9249b854a76fdf3e7fca6f56
dpetek21 <dpetek21@foi.hr>
2023-11-15 13:03:19 +0100

    Promjena funkcije gumba Register u activity Login na preusmjeravanje na activity Register. #3

--- Software/app/src/main/java/hr/foi/rampu/walktalk/LoginActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/LoginActivity.kt
@@ -1,11 +1,22 @@
 package hr.foi.rampu.walktalk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login2)
+        val register : Button = findViewById(R.id.LoginRegisterButton)
+        register.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+        val login : Button = findViewById(R.id.loginButton)
+        login.setOnClickListener{
+
+        }
     }
 }
