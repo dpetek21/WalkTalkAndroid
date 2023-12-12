1e833bedf46f202871741d71479d9cf72d205309
Marin Puškadija <mpuskadij20@foi.hr>
2023-12-12 18:10:05 +0100

    Dodaj klasu Event za rad s događajima. #6

---
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/entities/Event.kt
@@ -0,0 +1,15 @@
+package hr.foi.rampu.walktalk.entities
+
+import com.google.firebase.firestore.GeoPoint
+import java.util.Date
+
+class Event(
+    val name : String,
+    val numberOfKilometers : Double,
+    val numberOfPeople : Int,
+    val pace: String,
+    val date: Date,
+    val route: List<GeoPoint>,
+    val organizer: String,
+    val isPublic : Boolean
+)
