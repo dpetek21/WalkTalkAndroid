2d9bac21717bc0f81fe828bf34d2dbfc6f418827
dpetek21 <dpetek21@foi.hr>
2023-12-31 16:34:03 +0100

    Definicija vrijednosti u rutama i mock punjac ruta #10

--- Software/app/src/main/java/hr/foi/rampu/walktalk/entities/Route.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/entities/Route.kt
@@ -1,5 +1,11 @@
 package hr.foi.rampu.walktalk.entities
-
+import org.osmdroid.util.GeoPoint
 data class Route(
-    val name: String
-)
+    val name: String,
+    val start:GeoPoint,
+    val end:GeoPoint,
+    val rating:Int
+){
+    constructor(name: String, startLatitude: Double, startLongitude: Double, endLatitude: Double, endLongitude: Double, rating: Int)
+            : this(name, GeoPoint(startLatitude, startLongitude), GeoPoint(endLatitude, endLongitude), rating)
+}
---
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/helpers/MockRoute.kt
@@ -0,0 +1,14 @@
+package hr.foi.rampu.walktalk.helpers
+
+import hr.foi.rampu.walktalk.entities.Route
+import org.osmdroid.util.GeoPoint
+
+object MockRoute {
+    fun giveMockRoutes(): List<Route> {
+        val route1 = Route("First route", GeoPoint(37.7749, -122.4194), GeoPoint(37.7899, -122.4094), 1)
+        val route2 = Route("Second route - harder", GeoPoint(34.0522, -118.2437), GeoPoint(34.0722, -118.2637), 2)
+        val route3 = Route("Third route - hardest", GeoPoint(40.7128, -74.0060), GeoPoint(40.7228, -74.0160), 3)
+
+        return listOf(route1, route2, route3)
+    }
+}
