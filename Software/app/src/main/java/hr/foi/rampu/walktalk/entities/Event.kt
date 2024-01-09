9f76d15388821468fda5b97f63d63b5552511710
Marin Puškadija <mpuskadij20@student.foi.hr>
2024-01-09 19:37:27 +0100

    Dodaj atribut "organizer" u klasu Event i promijeni ju u data klasu. #6

--- Software/app/src/main/java/hr/foi/rampu/walktalk/entities/Event.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/entities/Event.kt
@@ -3,12 +3,13 @@ package hr.foi.rampu.walktalk.entities
 import com.google.firebase.firestore.GeoPoint
 import java.util.Date
 
-class Event(
+data class Event(
     val name : String,
     val numberOfKilometers : Double,
     val numberOfPeople : Int,
     val pace: String,
     val date: Date,
+    val organizer: String,
     val route: List<GeoPoint>?,
     val isPublic : Boolean
 )
