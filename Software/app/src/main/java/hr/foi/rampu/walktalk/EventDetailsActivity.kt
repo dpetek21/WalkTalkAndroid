969626f579bfc7481547469a493eae6790548ee4
Marin Puškadija <mpuskadij20@student.foi.hr>
2024-01-11 16:01:52 +0100

    Dodaj osluškivač na strelicu natrag da uništi aktivnost #6

--- Software/app/src/main/java/hr/foi/rampu/walktalk/EventDetailsActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/EventDetailsActivity.kt
@@ -2,10 +2,21 @@ package hr.foi.rampu.walktalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class EventDetailsActivity : AppCompatActivity() {
+    private lateinit var backButton: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_details)
+
+
+        backButton = findViewById(R.id.img_event_details_back_arrow)
+
+        backButton.setOnClickListener{
+            this.finish()
+        }
     }
+
+
 }
