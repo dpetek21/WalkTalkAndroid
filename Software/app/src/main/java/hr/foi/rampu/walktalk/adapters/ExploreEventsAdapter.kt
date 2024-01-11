d11c152857eb3018d8e0ed69235b33c4559054e8
Marin Puškadija <mpuskadij20@student.foi.hr>
2024-01-11 15:52:41 +0100

    Dodaj pokretanje EventDetailsActivity kod adaptera za recyclerview #6

--- Software/app/src/main/java/hr/foi/rampu/walktalk/adapters/ExploreEventsAdapter.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/adapters/ExploreEventsAdapter.kt
@@ -1,10 +1,14 @@
 package hr.foi.rampu.walktalk.adapters
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import hr.foi.rampu.walktalk.EventDetailsActivity
 import hr.foi.rampu.walktalk.R
 import hr.foi.rampu.walktalk.entities.Event
 
@@ -16,6 +20,7 @@ class ExploreEventsAdapter(val eventList: List<Event>) : RecyclerView.Adapter<Ex
         val numberOfPeople: TextView
         val pace : TextView
         val date : TextView
+        val cardView: CardView
 
         init {
             eventName = view.findViewById(R.id.txtv_event_name_list_item)
@@ -23,6 +28,7 @@ class ExploreEventsAdapter(val eventList: List<Event>) : RecyclerView.Adapter<Ex
             numberOfPeople = view.findViewById(R.id.txtv_people)
             pace = view.findViewById(R.id.txtv_pace)
             date = view.findViewById(R.id.txtv_date)
+            cardView = view.findViewById((R.id.cv_event))
         }
 
         fun bind(event: Event) {
@@ -50,6 +56,18 @@ class ExploreEventsAdapter(val eventList: List<Event>) : RecyclerView.Adapter<Ex
 
     override fun onBindViewHolder(holder: ExploreEventsHolder, position: Int) {
         holder.bind(eventList[position])
+        holder.itemView.setOnClickListener {
+            val activity = it.context as AppCompatActivity
+            val intent = Intent(activity,EventDetailsActivity::class.java)
+            intent.putExtra("event_name",eventList[position].name)
+            intent.putExtra("event_date",eventList[position].date)
+            intent.putExtra("event_kilometers",eventList[position].numberOfKilometers)
+            intent.putExtra("event_people",eventList[position].numberOfPeople)
+            intent.putExtra("event_organizer",eventList[position].organizer)
+            intent.putExtra("event_pace",eventList[position].pace)
+            intent.putExtra("event_public",eventList[position].isPublic)
+            activity.startActivity(intent)
+        }
     }
 
 
