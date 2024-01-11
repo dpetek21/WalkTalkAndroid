178d7c5ba7e4c3476193edb3b6e1f586815be5aa
Marin Puškadija <mpuskadij20@student.foi.hr>
2024-01-11 15:51:35 +0100

    Dodaj EventDetailsActivity za prikaz pojedinosti događaja #6

---
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/EventDetailsActivity.kt
@@ -0,0 +1,11 @@
+package hr.foi.rampu.walktalk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EventDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_details)
+    }
+}
