4005f21e5ddd3048872d622e694901fd703128a0
Gabriel Vesel <gvesel20@foi.hr>
2024-01-13 23:03:39 +0100

    Implementiraj group chat adapter #9

--- Software/app/src/main/java/hr/foi/rampu/walktalk/GroupChatActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/GroupChatActivity.kt
@@ -6,8 +6,11 @@ import android.util.Log
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
+import hr.foi.rampu.walktalk.adapteri_za_chat.GroupChatAdapter
 import hr.foi.rampu.walktalk.adapteri_za_chat.MessageAdapter
 import hr.foi.rampu.walktalk.database.GroupChatDAO
 import hr.foi.rampu.walktalk.firebaseHandler.UserDataContainer
@@ -20,15 +23,17 @@ class GroupChatActivity : AppCompatActivity() {
     private lateinit var sendButton: ImageButton
     private lateinit var groupChatRecyclerView: RecyclerView
     private lateinit var receiver: String
+    private lateinit var sender: String
     private lateinit var messageList: ArrayList<Message>
-    private lateinit var messageAdapter: MessageAdapter
+    private lateinit var groupChatAdapter: GroupChatAdapter
     private lateinit var groupChatDAO: GroupChatDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_group_chat)
 
-        receiver = "TestChat"
+        sender = UserDataContainer.username
+        receiver = "Test"
         groupChatDAO = GroupChatDAO(receiver)
         groupChatText = findViewById(R.id.edt_group_chat_massage)
         sendButton = findViewById(R.id.groupChatImageButton_send)
@@ -36,16 +41,25 @@ class GroupChatActivity : AppCompatActivity() {
         messageList = ArrayList<Message>()
         messageList.clear()
 
+        lifecycleScope.launch {
+            messageList = groupChatDAO.fetchMessagesIntoAList()
+            groupChatAdapter = GroupChatAdapter(this@GroupChatActivity, messageList, sender)
+            groupChatRecyclerView.layoutManager = LinearLayoutManager(this@GroupChatActivity)
+            groupChatRecyclerView.adapter = groupChatAdapter
+        }
+
 
         sendButton.setOnClickListener {
             lifecycleScope.launch {
                 if(groupChatText.text.isNotEmpty()){
                     groupChatDAO.saveMessage(groupChatText.text.toString())
+                    messageList.add(Message(groupChatText.text.toString(), sender, receiver, Timestamp.now()))
+                    groupChatAdapter.notifyDataSetChanged()
+                    groupChatText.text.clear()
                 }
             }
         }
 
     }
-
 
 }
--- Software/app/src/main/java/hr/foi/rampu/walktalk/adapteri_za_chat/GroupChatAdapter.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/adapteri_za_chat/GroupChatAdapter.kt
@@ -1,6 +1,7 @@
 package hr.foi.rampu.walktalk.adapteri_za_chat
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -10,23 +11,58 @@ import hr.foi.rampu.walktalk.klase_za_chat.Message
 
 class GroupChatAdapter(val context: Context, val messageList: ArrayList<Message>, val sender: String) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        const val VIEW_TYPE_SENT_MESSAGE = 1
+        const val VIEW_TYPE_RECEIVED_MESSAGE = 2
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        return when (viewType) {
+            VIEW_TYPE_SENT_MESSAGE -> {
+                val view = LayoutInflater.from(context).inflate(R.layout.sent_message, parent, false)
+                SentMessageViewHolder(view)
+            }
+            VIEW_TYPE_RECEIVED_MESSAGE -> {
+                val view = LayoutInflater.from(context).inflate(R.layout.receive_message, parent, false)
+                ReceivedMessageViewHolder(view)
+            }
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return messageList.size
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val message = messageList[position]
+
+        when (holder.itemViewType) {
+            VIEW_TYPE_SENT_MESSAGE -> {
+                val sentMessageViewHolder = holder as SentMessageViewHolder
+                sentMessageViewHolder.sentMessage.text = message.message
+            }
+            VIEW_TYPE_RECEIVED_MESSAGE -> {
+                val receivedMessageViewHolder = holder as ReceivedMessageViewHolder
+                receivedMessageViewHolder.receivedMessage.text = message.message
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val message = messageList[position]
+        return if (message.sender == sender) {
+            VIEW_TYPE_SENT_MESSAGE
+        } else {
+            VIEW_TYPE_RECEIVED_MESSAGE
+        }
     }
 
     class SentMessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val sentMessage = itemView.findViewById<TextView>(R.id.txt_sent_massage)
     }
 
-    class ReceiveMessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    class ReceivedMessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val receivedMessage = itemView.findViewById<TextView>(R.id.txt_receive_massage)
     }
 
