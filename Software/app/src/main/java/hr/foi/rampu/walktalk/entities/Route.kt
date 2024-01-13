67f9535d9899b347caa8b67b86eb626fab0294c5
dpetek21 <dpetek21@foi.hr>
2024-01-13 15:14:36 +0100

    Dodano svojstvo ID u Routeama u int, te su promjenjeni mock podaci u MockRoute #10

--- Software/app/src/main/java/hr/foi/rampu/walktalk/entities/Route.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/entities/Route.kt
@@ -1,14 +1,14 @@
 package hr.foi.rampu.walktalk.entities
 import org.osmdroid.util.GeoPoint
 data class Route(
-    val id: Int?,
+    val id: String?,
     val name: String,
     val start:GeoPoint,
     val end:GeoPoint,
     val rating:Int,
     val owner:String
 ){
-    constructor(id:Int?,name: String, startLatitude: Double, startLongitude: Double, endLatitude: Double, endLongitude: Double, rating: Int,owner:String)
+    constructor(id:String?,name: String, startLatitude: Double, startLongitude: Double, endLatitude: Double, endLongitude: Double, rating: Int,owner:String)
             : this(id,name, GeoPoint(startLatitude, startLongitude), GeoPoint(endLatitude, endLongitude), rating, owner)
     constructor(name: String, startLatitude: Double, startLongitude: Double, endLatitude: Double, endLongitude: Double, rating: Int,owner:String)
             : this(null,name, GeoPoint(startLatitude, startLongitude), GeoPoint(endLatitude, endLongitude), rating, owner)
