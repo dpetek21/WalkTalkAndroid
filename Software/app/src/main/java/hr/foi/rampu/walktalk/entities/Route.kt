fe88077d8bdf71d6e4bb1ae17a05c24948c9a9c1
dpetek21 <dpetek21@foi.hr>
2024-01-13 14:23:20 +0100

    Dodano svojstvo ID u Route, te je dodan konstruktor koji to uzima u obzir  #10

--- Software/app/src/main/java/hr/foi/rampu/walktalk/entities/Route.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/entities/Route.kt
@@ -1,12 +1,16 @@
 package hr.foi.rampu.walktalk.entities
 import org.osmdroid.util.GeoPoint
 data class Route(
+    val id: Int?,
     val name: String,
     val start:GeoPoint,
     val end:GeoPoint,
     val rating:Int,
     val owner:String
 ){
+    constructor(id:Int?,name: String, startLatitude: Double, startLongitude: Double, endLatitude: Double, endLongitude: Double, rating: Int,owner:String)
+            : this(id,name, GeoPoint(startLatitude, startLongitude), GeoPoint(endLatitude, endLongitude), rating, owner)
     constructor(name: String, startLatitude: Double, startLongitude: Double, endLatitude: Double, endLongitude: Double, rating: Int,owner:String)
-            : this(name, GeoPoint(startLatitude, startLongitude), GeoPoint(endLatitude, endLongitude), rating, owner)
+            : this(null,name, GeoPoint(startLatitude, startLongitude), GeoPoint(endLatitude, endLongitude), rating, owner)
+
 }
