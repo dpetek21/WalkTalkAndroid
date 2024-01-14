0fe070c15fd961158351a8c64b9af042f7693c69
Gabriel Vesel <gvesel20@foi.hr>
2024-01-15 00:57:32 +0100

    Dodaj layout za pedometar #12

---
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/PedometerActivity.kt
@@ -0,0 +1,11 @@
+package hr.foi.rampu.walktalk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PedometerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pedometer)
+    }
+}
