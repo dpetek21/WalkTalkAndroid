87b4ead01ae1175125c9b8ff7bfc793c0e35ebe2
Marin Puškadija <mpuskadij20@student.foi.hr>
2024-01-14 17:36:11 +0100

    Promijeni NewEventDialogHelper da se konstrukira novi događaj tek kada je uneseno ime događaja te je dodaj osluškivač kod definiranja AlertDialog-a #6

--- Software/app/src/main/java/hr/foi/rampu/walktalk/EventsActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/EventsActivity.kt
@@ -2,16 +2,24 @@ package hr.foi.rampu.walktalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.appcompat.app.AlertDialog
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import hr.foi.rampu.walktalk.adapters.EventsPagerAdapter
+import hr.foi.rampu.walktalk.database.DatabaseEvent
+import hr.foi.rampu.walktalk.database.DatabaseFriend
+import hr.foi.rampu.walktalk.entities.Event
 import hr.foi.rampu.walktalk.helpers.NewEventDialogHelper
 import hr.foi.rampu.walktalk.helpers.Pace
 import hr.foi.rampu.walktalk.navigation.NavigationSetup
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class EventsActivity : AppCompatActivity() {
     private lateinit var tabLayout : TabLayout
@@ -51,10 +59,34 @@ class EventsActivity : AppCompatActivity() {
         AlertDialog.Builder(this)
             .setView(newTaskDialogView)
             .setTitle(getString(R.string.create_new_event))
-            .setPositiveButton("Create",null)
+            .setPositiveButton("Create") {dialog,_ ->
+                val eventName : EditText = newTaskDialogView.findViewById(R.id.et_new_event_name)
+                if (eventName.text.isEmpty()) {
+                    Log.i("EVENT_NAME_EMPTY", "Event name is empty")
+                } else {
+                    val sdfDate = SimpleDateFormat("dd.MM.yyyy.", Locale.US)
+                    val spinnerPace = newTaskDialogView.findViewById<Spinner>(R.id.spn_pace)
+                    val dateSelection = newTaskDialogView.findViewById<EditText>(R.id.et_event_date)
+                    val event = Event(eventName.text.toString(),
+                        0.0,
+                        0,
+                        spinnerPace.selectedItem as String,
+                        sdfDate.parse(dateSelection.text.toString()),
+                        DatabaseFriend.username,
+                        null,
+                        true,
+                        null
+                        )
+                    dialog.dismiss()
+
+                    DatabaseEvent.addNewEvent(event)
+                }
+                return@setPositiveButton
+            }
             .setNegativeButton("Cancel",null)
             .create()
             .show()
+
 
         val dialogHelper = NewEventDialogHelper(newTaskDialogView)
         dialogHelper.populateSpinnerPace(Pace.getAllPaces())
--- Software/app/src/main/java/hr/foi/rampu/walktalk/helpers/NewEventDialogHelper.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/helpers/NewEventDialogHelper.kt
@@ -7,17 +7,12 @@ import android.widget.Spinner
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.datepicker.MaterialDatePicker
 import hr.foi.rampu.walktalk.R
-import hr.foi.rampu.walktalk.database.DatabaseEvent
-import hr.foi.rampu.walktalk.database.DatabaseFriend
-import hr.foi.rampu.walktalk.entities.Event
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class NewEventDialogHelper(private val view: View) {
     private val sdfDate = SimpleDateFormat("dd.MM.yyyy.", Locale.US)
     private val spinnerPace = view.findViewById<Spinner>(R.id.spn_pace)
-    private val etEventName = view.findViewById<EditText>(R.id.et_new_event_name)
-    private val spinnerRoute = view.findViewById<Spinner>(R.id.spn_route)
     private val dateSelection = view.findViewById<EditText>(R.id.et_event_date)
 
     fun populateSpinnerPace(paces: List<String>) {
@@ -29,6 +24,7 @@ class NewEventDialogHelper(private val view: View) {
         spinnerPace.adapter = spinnerAdapter
     }
 
+    /*
     private fun buildEvent() : Event
     {
         val eventName = etEventName.text.toString()
@@ -38,6 +34,8 @@ class NewEventDialogHelper(private val view: View) {
 
         return Event(eventName,0.0,0, pace, sdfDate.parse(dateSelection.getText().toString())!!, organizer,null,true)
     }
+
+     */
 
     fun activateDateListener(supportFragmentManager: FragmentManager) {
         dateSelection.setOnFocusChangeListener{view, hasFocus ->
@@ -50,8 +48,8 @@ class NewEventDialogHelper(private val view: View) {
                 picker.show(supportFragmentManager, "EVENT_DATE")
                 picker.addOnPositiveButtonClickListener {
                     dateSelection.setText(sdfDate.format(it).toString())
-                    val newEvent = buildEvent()
-                    DatabaseEvent.addNewEvent(newEvent)
+                    //val newEvent = buildEvent()
+                    //DatabaseEvent.addNewEvent(newEvent)
                 }
                 view.clearFocus()
             }
