3f007ba8ec501293369f63e61d2b4222ff522471
Gabriel Vesel <gvesel20@foi.hr>
2024-01-14 16:34:47 +0100

    Napravi prikaz razgovora iz firestore baze u chats list activity #9

--- Software/app/src/main/java/hr/foi/rampu/walktalk/ChatsListActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/ChatsListActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import hr.foi.rampu.walktalk.adapteri_za_chat.ChatsListAdapter
 import hr.foi.rampu.walktalk.database.ChatsListDAO
 import kotlinx.coroutines.launch
 
@@ -14,18 +16,30 @@ private lateinit var privateChatsRecyclerView: RecyclerView
 private lateinit var privateChatList: List<String>
 private lateinit var groupChatList: List<String>
 private lateinit var chatsListDAO: ChatsListDAO
+private lateinit var privateChatsListAdapter: ChatsListAdapter
+private lateinit var groupChatsListAdapter: ChatsListAdapter
 
 class ChatsListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chats_list)
 
+        privateChatsRecyclerView = findViewById(R.id.privateChatsRecyclerView)
+        groupsRecyclerView = findViewById(R.id.groupChatsRecyclerView)
+
         chatsListDAO = ChatsListDAO()
         lifecycleScope.launch {
             privateChatList = chatsListDAO.fetchPrivateChatsIntoAList()
             groupChatList = chatsListDAO.fetchGroupChatsIntoAList()
-            Log.i("private", privateChatList[0])
-            Log.i("group", groupChatList[0])
+
+            privateChatsListAdapter = ChatsListAdapter(this@ChatsListActivity, privateChatList)
+            groupChatsListAdapter = ChatsListAdapter(this@ChatsListActivity, groupChatList)
+
+            privateChatsRecyclerView.layoutManager = LinearLayoutManager(this@ChatsListActivity)
+            privateChatsRecyclerView.adapter = privateChatsListAdapter
+
+            groupsRecyclerView.layoutManager = LinearLayoutManager(this@ChatsListActivity)
+            groupsRecyclerView.adapter = groupChatsListAdapter
         }
 
     }
--- Software/app/src/main/java/hr/foi/rampu/walktalk/adapteri_za_chat/ChatsListAdapter.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/adapteri_za_chat/ChatsListAdapter.kt
@@ -1,21 +1,35 @@
 package hr.foi.rampu.walktalk.adapteri_za_chat
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import hr.foi.rampu.walktalk.R
 
 class ChatsListAdapter(val context: Context, val chatsList: List<String>):
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+    RecyclerView.Adapter<ChatsListAdapter.ChatViewHolder>() {
+
+    inner class ChatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val chatTitleTextView: TextView = itemView.findViewById(R.id.txt_chat_list_item_name)
+        val dividerView: View = itemView.findViewById(R.id.dividerView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.chats_list_item, parent, false)
+        return ChatViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return chatsList.size
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
+        val chatTitle = chatsList[position]
+        holder.chatTitleTextView.text = chatTitle
+
+        holder.dividerView.visibility = if (position == chatsList.size - 1) View.GONE else View.VISIBLE
     }
 
 }
