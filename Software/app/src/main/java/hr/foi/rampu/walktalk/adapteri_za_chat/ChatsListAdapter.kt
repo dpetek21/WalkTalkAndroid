9e1ba956c05ff8ad37742633a52b391e5e34c5e1
Gabriel Vesel <gvesel20@foi.hr>
2024-01-14 15:56:20 +0100

    Napravi kostur za chats list adapter #9

---
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/adapteri_za_chat/ChatsListAdapter.kt
@@ -0,0 +1,21 @@
+package hr.foi.rampu.walktalk.adapteri_za_chat
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ChatsListAdapter(val context: Context, val chatsList: List<String>):
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
