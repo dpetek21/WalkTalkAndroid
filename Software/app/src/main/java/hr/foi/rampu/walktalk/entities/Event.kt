48534b705989fa484be993548531f6a7cb2b5061
Marin Puškadija <mpuskadij20@student.foi.hr>
2024-01-14 17:33:00 +0100

    Promijeni val u var za atribute događaja koji su promjenjivi. #6

--- Software/app/src/main/java/hr/foi/rampu/walktalk/entities/Event.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/entities/Event.kt
@@ -4,11 +4,11 @@ import com.google.firebase.firestore.GeoPoint
 import java.util.Date
 
 data class Event(
-    val name : String = "",
+    var name : String = "",
     val numberOfKilometers : Double = 0.0,
     val numberOfPeople : Int = 0,
-    val pace: String = "",
-    val date:Date? = null,
+    var pace: String = "",
+    var date:Date? = null,
     val organizer: String = "",
     val route: List<GeoPoint>? = null,
     val isPublic : Boolean = true,
