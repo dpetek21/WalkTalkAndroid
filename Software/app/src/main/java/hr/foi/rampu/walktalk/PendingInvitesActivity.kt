09804c97852dda4ff04685148b75ad3cad2e3b63
Marin Puškadija <mpuskadij20@foi.hr>
2024-01-18 17:39:52 +0100

    Dodaj PendingInvitesActivity za prikaz zahtjeva koje je potrebno odobriti ili odbiti #13

---
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/PendingInvitesActivity.kt
@@ -0,0 +1,11 @@
+package hr.foi.rampu.walktalk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PendingInvitesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pending_invites)
+    }
+}
