777e523f47705cb872aae3d5445315f0a14bd49a
Marin Puškadija <mpuskadij20@foi.hr>
2024-01-18 18:16:25 +0100

    Dodaj PendingInvitesAdapter za recyclerview pozivnica te pripremi variajblu recyclerView za povezivanje adaptera #13

--- Software/app/src/main/java/hr/foi/rampu/walktalk/PendingInvitesActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/PendingInvitesActivity.kt
@@ -2,10 +2,16 @@ package hr.foi.rampu.walktalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class PendingInvitesActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pending_invites)
+
+        recyclerView = findViewById(R.id.rv_pending_invites)
+
+
     }
 }
---
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/adapters/PendingInvitesAdapter.kt
@@ -0,0 +1,43 @@
+package hr.foi.rampu.walktalk.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import hr.foi.rampu.walktalk.R
+
+class PendingInvitesAdapter(val invitesList: ArrayList<String>) : RecyclerView.Adapter<PendingInvitesAdapter.PendingInvitesViewHolder>() {
+    inner class PendingInvitesViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+        val username : TextView
+        val btnAccept : Button
+        val btnDecline : Button
+
+        init {
+            username = view.findViewById(R.id.txtv_username_pending_friend)
+            btnAccept = view.findViewById(R.id.btn_accept)
+            btnDecline = view.findViewById(R.id.btn_decline)
+        }
+
+        fun bind(user: String)
+        {
+            username.text = user
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PendingInvitesAdapter.PendingInvitesViewHolder {
+        val invitesView = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.pending_invite_item, parent, false)
+        return PendingInvitesViewHolder(invitesView)
+    }
+
+    override fun getItemCount(): Int {
+        return invitesList.size
+    }
+
+    override fun onBindViewHolder(holder: PendingInvitesViewHolder, position: Int) {
+        holder.bind(invitesList[position])
+    }
+}
