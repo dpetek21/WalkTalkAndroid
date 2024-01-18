ac9d1aec66a2de3c33416669b5a50f0f732b0977
Gabriel Vesel <gvesel20@foi.hr>
2024-01-18 09:42:20 +0100

    Kreiraj PedometerService #12 Foreground servis za pedometar

---
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/services/PedometerService.kt
@@ -0,0 +1,12 @@
+package hr.foi.rampu.walktalk.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class PedometerService: Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+}
