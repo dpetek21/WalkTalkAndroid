d73b9a5ff11ce9c8bef5660d5f79d69a10eabd30
Gabriel Vesel <gvesel20@foi.hr>
2024-01-18 09:56:49 +0100

    Kreiraj notifikaciju servisa #12

--- Software/app/src/main/java/hr/foi/rampu/walktalk/services/PedometerService.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/services/PedometerService.kt
@@ -3,10 +3,33 @@ package hr.foi.rampu.walktalk.services
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import hr.foi.rampu.walktalk.R
 
 class PedometerService: Service() {
     override fun onBind(p0: Intent?): IBinder? {
         return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when(intent?.action){
+            Actions.START.toString() -> start()
+            Actions.STOP.toString() -> stopSelf()
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun start(){
+        val notification = NotificationCompat.Builder(this, "pedometer_channel")
+            .setSmallIcon(R.drawable.ic_attending_event)
+            .setContentTitle("Pedometer service")
+            .setContentText("Number of steps: 20")
+            .build()
+        startForeground(1, notification)
+    }
+
+    enum class Actions{
+        START, STOP
     }
 
 }
