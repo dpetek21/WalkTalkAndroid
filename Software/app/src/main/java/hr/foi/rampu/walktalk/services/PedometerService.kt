87214a307b30ac6a15d4c0f3da737e1a47d0d42c
Gabriel Vesel <gvesel20@foi.hr>
2024-01-25 10:55:07 +0100

    Implementiraj pedometar foreground servis koji broji korake #12

--- Software/app/src/main/java/hr/foi/rampu/walktalk/services/PedometerService.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/services/PedometerService.kt
@@ -3,13 +3,24 @@ package hr.foi.rampu.walktalk.services
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Build
 import android.os.IBinder
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import hr.foi.rampu.walktalk.R
 
-class PedometerService: Service() {
+class PedometerService: Service(), SensorEventListener {
+
+    private lateinit var sensorManager: SensorManager
+    private var totalSteps = 0
+    private var initialStepCount = 0
+    private var initialStepsRecorded = false
     override fun onBind(p0: Intent?): IBinder? {
         return null
     }
@@ -28,6 +39,24 @@ class PedometerService: Service() {
     override fun onCreate() {
         super.onCreate()
         createNotificationChannel()
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if (stepSensor == null) {
+            stopSelf()
+        } else {
+            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_GAME)
+        }
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val serviceIntent = Intent(applicationContext, PedometerService::class.java)
+        serviceIntent.action = PedometerService.Actions.STOP.toString()
+        stopService(serviceIntent)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -42,13 +71,38 @@ class PedometerService: Service() {
         val notification = NotificationCompat.Builder(this, "pedometer_channel")
             .setSmallIcon(R.drawable.ic_attending_event)
             .setContentTitle("Pedometer service")
-            .setContentText("Number of steps: 20")
+            .setContentText("Number of steps: $totalSteps")
             .build()
         startForeground(1, notification)
     }
 
+    private fun updateNotification() {
+        val notification = NotificationCompat.Builder(this, "pedometer_channel")
+            .setSmallIcon(R.drawable.ic_attending_event)
+            .setContentTitle("Pedometer service")
+            .setContentText("Number of steps: $totalSteps")
+            .build()
+        startForeground(1, notification)
+    }
+
+
     enum class Actions{
         START, STOP
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_STEP_COUNTER) {
+            if(!initialStepsRecorded){
+                initialStepCount = event.values[0].toInt()
+                initialStepsRecorded = true
+            }
+            totalSteps = event.values[0].toInt() - initialStepCount
+            updateNotification()
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
     }
 
 }
