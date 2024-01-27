93478c9eeb634210d49e1bedccfe376af02f08da
Gabriel Vesel <gvesel20@foi.hr>
2024-01-27 15:39:53 +0100

    Kreiraj fragmente za leaderboard #11

---
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/fragments/WeekLeaderboardFragment.kt
@@ -0,0 +1,20 @@
+package hr.foi.rampu.walktalk.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import hr.foi.rampu.walktalk.R
+
+class WeekLeaderboardFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_week_leaderboard, container, false)
+    }
+
+}
