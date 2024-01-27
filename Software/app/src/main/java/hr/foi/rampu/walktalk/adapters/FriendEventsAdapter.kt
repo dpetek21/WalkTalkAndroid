e04f4e23e0a7372dd6f4f3b27ca584e0bbdc87d8
Marin Puškadija <mpuskadij20@foi.hr>
2024-01-27 11:57:03 +0100

    -Dodaj FriendEventsAdapter i poveži ga s FriendEventsFragment-om #13

---
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/adapters/FriendEventsAdapter.kt
@@ -0,0 +1,79 @@
+package hr.foi.rampu.walktalk.adapters
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import hr.foi.rampu.walktalk.EventDetailsActivity
+import hr.foi.rampu.walktalk.R
+import hr.foi.rampu.walktalk.database.DatabaseEvent
+import hr.foi.rampu.walktalk.entities.Event
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class FriendEventsAdapter(val eventList: List<Event>) : RecyclerView.Adapter<FriendEventsAdapter.FriendEventsHolder>() {
+    inner class FriendEventsHolder(view : View) : RecyclerView.ViewHolder(view) {
+        val eventName: TextView
+        val kilometers: TextView
+        val numberOfPeople: TextView
+        val pace: TextView
+        val date: TextView
+        val cardView: CardView
+
+        init {
+            eventName = view.findViewById(R.id.txtv_event_name_list_item)
+            kilometers = view.findViewById(R.id.txtv_kilometres)
+            numberOfPeople = view.findViewById(R.id.txtv_people)
+            pace = view.findViewById(R.id.txtv_pace)
+            date = view.findViewById(R.id.txtv_date)
+            cardView = view.findViewById((R.id.cv_event))
+        }
+
+        fun bind(event: Event) {
+            val sdfDate = SimpleDateFormat("dd.MM.yyyy.", Locale.US)
+            eventName.text = event.name
+            val kilo = event.numberOfKilometers.toString() + " km"
+            kilometers.text = kilo
+            if (event.acceptedInvites != null) {
+                numberOfPeople.text = buildString {
+                    append(event.acceptedInvites!!.size.toString())
+                    append(" people coming")
+                }
+            } else {
+                numberOfPeople.text = buildString {
+                    append(0.toString())
+                    append(" people coming")
+                }
+            }
+            val pc = event.pace + " pace"
+            pace.text = pc
+            date.text = sdfDate.format(event.date!!)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FriendEventsHolder {
+        val friendEventsView = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.event_list_item, parent, false)
+        return FriendEventsHolder(friendEventsView)
+    }
+
+    override fun getItemCount(): Int {
+        return eventList.size
+    }
+
+    override fun onBindViewHolder(holder: FriendEventsHolder, position: Int) {
+        holder.bind(eventList[position])
+        holder.itemView.setOnClickListener {
+            val activity = it.context as AppCompatActivity
+            val intent = Intent(activity, EventDetailsActivity::class.java)
+            DatabaseEvent.event = eventList[position]
+            activity.startActivity(intent)
+        }
+    }
+}
--- Software/app/src/main/java/hr/foi/rampu/walktalk/fragments/FriendEventsFragment.kt
+++ Software/app/src/main/java/hr/foi/rampu/walktalk/fragments/FriendEventsFragment.kt
@@ -5,13 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rampu.walktalk.R
+import hr.foi.rampu.walktalk.adapters.FriendEventsAdapter
+import hr.foi.rampu.walktalk.database.DatabaseEvent
+import hr.foi.rampu.walktalk.entities.Event
+import kotlinx.coroutines.launch
 
 class FriendEventsFragment : Fragment() {
-
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    private suspend fun getFriendEvents() {
+        val friendEventsList : List<Event> = DatabaseEvent.getFriendEvents()
+        recyclerView = requireView().findViewById(R.id.rv_friend_events)
+        recyclerView.adapter = FriendEventsAdapter(friendEventsList)
+        recyclerView.layoutManager = LinearLayoutManager(requireView().context)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch {
+            getFriendEvents()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lifecycleScope.launch {
+            getFriendEvents()
+        }
     }
 
     override fun onCreateView(
