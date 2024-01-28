33a72c9a61041a9c43161141340bf0490d3027fa
Marin Puškadija <mpuskadij20@foi.hr>
2024-01-28 15:12:47 +0100

    Dodaj dependecy za Firestore cloud messaging #7

--- Software/app/build.gradle.kts
+++ Software/app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation ("org.osmdroid:osmdroid-android:6.1.12")
     implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.google.firebase:firebase-messaging:23.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
